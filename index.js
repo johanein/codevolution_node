dc8cdce55a4ebbee4a4f524870953d138f79aae8
Albert <johanein@gmail.com>
2023-02-16 15:11:54 +0530

    added env PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello world");
 });
-
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log("server listening in 3000");
 });
